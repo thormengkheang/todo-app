677702988daa3d413a3881dfc6d2f840983bd849
Mengkheang Thor <thormengkheang@gmail.com>
2023-05-16 10:22:16 +0700

    add expo linking

--- App.tsx
+++ App.tsx
@@ -2,12 +2,29 @@ import { NavigationContainer } from "@react-navigation/native";
 import MainStackScreen from "./navigation/MainStackScreen";
 import { TodoProvider } from "./Todo.context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as Linking from "expo-linking";
+import { Text } from "react-native";
+
+const prefix = Linking.createURL("/");
 
 export default function App() {
+  const linking = {
+    prefixes: [prefix, 'nham24app://'],
+    config: {
+      screens: {
+        AddTodo: "todo/add/:title?",
+        EditTodo: "todo/edit/:id",
+      },
+    },
+  };
+
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <TodoProvider>
-        <NavigationContainer>
+        <NavigationContainer
+          linking={linking}
+          fallback={<Text>Loading...</Text>}
+        >
           <MainStackScreen />
         </NavigationContainer>
       </TodoProvider>
